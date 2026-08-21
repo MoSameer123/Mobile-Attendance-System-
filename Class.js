a8d98131790ec8de3fb0a82edeb47a700267766b
Mo Sameer <mosameerr312@gmail.com>
2026-08-21 18:39:49 +0300

    Define ClassSchema with geolocation fields
    Added latitude, longitude, and radius fields to ClassSchema.

---
+++ Class.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const ClassSchema = new mongoose.Schema({
+  name:String,
+  teacherId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  subject: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subject",
+    required: true,
+  },
+
+  students: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  // 🔥 ADD THIS
+  latitude: {
+    type: Number,
+    required: true
+  },
+
+  longitude: {
+    type: Number,
+    required: true
+  },
+  radius: {
+    type: Number,
+    default: 50,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Class", ClassSchema);
