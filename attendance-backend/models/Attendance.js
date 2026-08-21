321834d0fa4fe3691b3d1bbc8ba8a131def6ea5c
Mo Sameer <mosameerr312@gmail.com>
2026-08-21 18:35:02 +0300

    Add Attendance schema with validation and indexing

--- attendance-backend/models/Attendance.js
+++ attendance-backend/models/Attendance.js
@@ -1,1 +1,52 @@
+const mongoose = require("mongoose");
 
+const AttendanceSchema = new mongoose.Schema({
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  latitude: Number,
+  longitude: Number,
+  //distance : Number,
+  status:{ 
+    type: String,
+    enum: ['present', 'absent'],
+    required: true
+  },
+  //location status
+  locationStatus: {
+  type: String,
+  enum: ["INSIDE", "OUTSIDE", "BOUNDARY"],
+  required: true
+},
+  classId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Class",
+    required: true
+  },
+
+  subjectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Subject",
+    required: true
+  },
+  //addddd
+  subjectName:{
+    type:String,
+    required:true,
+  }
+
+});
+
+// 🔒 Prevent duplicate attendance
+AttendanceSchema.index(
+  { student: 1, subjectId: 1, date: 1 },
+  { unique: true }
+);
+
+module.exports = mongoose.model("Attendance", AttendanceSchema);
